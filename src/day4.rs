2f4c0306c4ad829609e38650b984a35d168c48b2
vzikratyi <vzikratyi@playtika.com>
2024-12-05 00:40:12 +0200

    Day 4. Part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,40 +1,12 @@
 use std::error::Error;
-use std::fmt::format;
 use advent_tools::fetch_data;
-use regex::Regex;
 
-const XMAS: &str = "XMAS";
+const MAS: &str = "MAS";
 
 pub async fn execute() -> Result<(), Box<dyn Error>> {
     // 2596 - too high
     let url = "https://adventofcode.com/2024/day/4/input";
     let data = fetch_data(url).await?;
-    // let data: Vec<String> = r"MMMSXXMASM
-    //                     MSAMXMSMSA
-    //                     AMXSXMAAMM
-    //                     MSAMASMSMX
-    //                     XMASAMXAMM
-    //                     XXAMMXXAMA
-    //                     SMSMSASXSS
-    //                     SAXAMASAAA
-    //                     MAMMMXMMMM
-    //                     MXMXAXMASX"
-    //     .lines()
-    //     .map(|s| s.trim().to_string())
-    //     .collect();
-    // let data: Vec<String> = r"....XXMAS.
-    //                         .SAMXMS...
-    //                         ...S..A...
-    //                         ..A.A.MS.X
-    //                         XMASAMX.MM
-    //                         X.....XA.A
-    //                         S.S.S.S.SS
-    //                         .A.A.A.A.A
-    //                         ..M.M.M.MM
-    //                         .X.X.XMASX"
-    //     .lines()
-    //     .map(|s| s.trim().to_string())
-    //     .collect();
 
     let mut data_arr: Vec<Vec<char>> = data.into_iter()
         .map(|s| {
@@ -49,42 +21,55 @@ pub async fn execute() -> Result<(), Box<dyn Error>> {
     data_arr.push(vec!['*'; row_length]);
     data_arr.insert(0, vec!['*'; row_length]);
 
+    let mut data_heat_map: Vec<Vec<char>> = data_arr.iter()
+        .map(|row| row.iter().map(|_| '0').collect())
+        .collect();
+
+    for i in 0..data_arr.len() {
+        for j in 0..data_arr[0].len() {
+            find_x_mas(i, j, &data_arr, &mut data_heat_map, 0, -1, -1);
+            find_x_mas(i, j, &data_arr, &mut data_heat_map, 0, -1, 1);
+            find_x_mas(i, j, &data_arr, &mut data_heat_map, 0, 1, -1);
+            find_x_mas(i, j, &data_arr, &mut data_heat_map, 0, 1, 1);
+        }
+    }
     let mut xmas_count = 0;
     for i in 0..data_arr.len() {
         for j in 0..data_arr[0].len() {
-            xmas_count += count_xmas(i, j, &data_arr, 0, -1, -1);
-            xmas_count += count_xmas(i, j, &data_arr, 0, -1, 0);
-            xmas_count += count_xmas(i, j, &data_arr, 0, -1, 1);
-            xmas_count += count_xmas(i, j, &data_arr, 0, 0, -1);
-            xmas_count += count_xmas(i, j, &data_arr, 0, 0, 1);
-            xmas_count += count_xmas(i, j, &data_arr, 0, 1, -1);
-            xmas_count += count_xmas(i, j, &data_arr, 0, 1, 0);
-            xmas_count += count_xmas(i, j, &data_arr, 0, 1, 1);
+            if data_arr[i][j] == 'A' && data_heat_map[i][j] == '2' {
+                xmas_count += 1;
+            }
         }
     }
+    print(&data_heat_map);
     print(&data_arr);
-    println!("xmas_count: {}", xmas_count);
+    println!("x_mas_count: {}", xmas_count);
 
     Ok(())
 }
 
-fn count_xmas(i: usize, j: usize, data_arr: &Vec<Vec<char>>, xmas_char_index: usize, i_dir: i32, j_dir: i32) -> i32 {
+fn find_x_mas(i: usize, j: usize, data_arr: &Vec<Vec<char>>, data_heat_map: &mut Vec<Vec<char>>, xmas_char_index: usize, i_dir: i32, j_dir: i32) -> i32 {
     if i < 0 || j < 0 || i >= data_arr.len() || j >= data_arr[i].len() {
         return 0;
     }
     let current_char = data_arr[i][j];
-    let expected_char = XMAS.chars().nth(xmas_char_index).unwrap();
+    let expected_char = MAS.chars().nth(xmas_char_index).unwrap();
     if current_char != expected_char {
         return 0;
     }
 
     let next_xmas_char_index = xmas_char_index + 1;
-    if next_xmas_char_index == XMAS.len() {
+    if next_xmas_char_index == MAS.len() {
+        data_heat_map[i][j] = char::from_u32((data_heat_map[i][j] as u32) + 1).unwrap();
         return 1;
     }
     let next_i = (i as i32 + i_dir) as usize;
     let next_j = (j as i32 + j_dir) as usize;
-    let res = count_xmas(next_i, next_j, data_arr, next_xmas_char_index, i_dir, j_dir);
+    let res = find_x_mas(next_i, next_j, data_arr, data_heat_map, next_xmas_char_index, i_dir, j_dir);
+
+    if res > 0 {
+        data_heat_map[i][j] = char::from_u32((data_heat_map[i][j] as u32) + 1).unwrap();
+    }
 
     res
 }
